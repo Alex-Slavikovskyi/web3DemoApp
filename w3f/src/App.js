8479f9c437a91cc07cc04dc884181541234ea7b7
Artem Shevchuk <art.shev00@gmail.com>
2023-07-06 23:13:10 +0200

    first setup

---
+++ w3f/src/App.js
@@ -0,0 +1,21 @@
+import { Box } from '@mui/material';
+
+function App() {
+  return (
+    <Box
+      sx={{
+        backgroundColor: '#5B735E',
+        w: '100vw',
+        height: '1000px',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: '35px',
+      }}
+    >
+      <Box>WELKOME KURVA, ARE YOU READY ???</Box>
+    </Box>
+  );
+}
+
+export default App;
