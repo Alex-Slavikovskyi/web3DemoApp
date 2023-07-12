93cc27d50b4d4af660183e90144565e928b3d4b7
Artem Shevchuk <art.shev00@gmail.com>
2023-07-12 20:42:22 +0200

    added footer

--- w3f/src/layout/Layout.jsx
+++ w3f/src/layout/Layout.jsx
@@ -1,18 +1,20 @@
 import { Box, Container } from '@mui/material';
 import React from 'react';
 import Heder from '../components/Heder';
-import { Outlet } from "react-router-dom";
+import { Outlet } from 'react-router-dom';
 import CssBaseline from '@mui/material/CssBaseline';
 import '../style/main.scss';
-
+import Footer from '../components/Footer';
 
 function Layout() {
   return (
     <CssBaseline>
-      <Box sx={{backgroundColor: '#0C1115', width: '1320px', margin: '0 auto'}}>
+      <Box
+        sx={{ backgroundColor: '#0C1115', width: '1320px', margin: '0 auto' }}
+      >
         <Heder />
         <Outlet />
-
+        <Footer />
       </Box>
     </CssBaseline>
   );
