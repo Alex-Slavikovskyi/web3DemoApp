a6e97bab04ac656bd8808374318502f87856cfef
Alex <yosya1989@gmail.com>
2023-07-16 20:18:26 +0200

    create video menu

---
+++ w3f/src/components/MenuVideo.jsx
@@ -0,0 +1,57 @@
+import { Box, Tab, Tabs, styled } from '@mui/material'
+import React, { useState } from 'react'
+import { VideoPreview } from './VideoPreview';
+import { previewVideoContent } from './data/previewVideoContent';
+
+
+
+const CustomTab = styled(Tab)((props) => ({
+  fontWeight: '600',
+  fontSize: '14px',
+  width: '50%',
+  textTransform: 'capitalize',
+  textDecoration: 'blink',
+  border: '1px solid #3B4753',
+  color: '#FFF',
+  '&:hover': {
+    cursor: 'pointer',
+  },
+  '&.Mui-selected': {
+    backgroundColor: '#55E5C5',
+    color: '#131A21',
+  },
+}));
+
+
+export const MenuVideo = ({setCurrentVideo}) => {
+  const [tabIndex, setTabIndex] = useState(0);
+
+  const handleTabChange = (event, newTabIndex) => {
+    setTabIndex(newTabIndex);
+  };
+
+  return (
+    <Box sx={{
+      color: '#FFF',
+      backgroundColor: "#202932",
+      width: '100%',
+      padding: '18px 12px'
+    }}>
+
+
+      <Tabs value={tabIndex} onChange={handleTabChange} indicatorColor='none' >
+        <CustomTab label="Suggested journeys" key='Suggested' />
+        <CustomTab label="More from brand" key='More' />
+      </Tabs>
+
+{
+  previewVideoContent && previewVideoContent.map(preview => (
+    <VideoPreview preview={preview} setCurrentVideo={setCurrentVideo} key={preview.id} />
+  ))
+}
+      
+
+
+    </Box>
+  )
+}
---
+++ w3f/src/components/VideoPreview.jsx
@@ -0,0 +1,44 @@
+import { Box, CardMedia, Typography } from '@mui/material';
+import React from 'react';
+import StarIcon from '@mui/icons-material/Star';
+
+export const VideoPreview = ({ preview, setCurrentVideo }) => {
+  return (
+    <Box sx={{
+      padding: '2px', marginTop: '16px', display: 'flex', gap: '10px',
+      '&:hover': {
+        cursor: 'pointer',
+        backgroundColor: '#3A3F43'
+      }
+    }}
+    onClick={() => setCurrentVideo(preview)}
+    >
+      <CardMedia
+        component="img"
+        alt="prev"
+        image={preview.prevImg}
+        sx={{
+          width: '134px',
+          height: '84px',
+        }}
+      />
+      <Box sx={{ paddingTop: '6px', display: 'flex', flexDirection: 'column', gap: '6px' }}>
+        <Typography sx={{ fontWeight: 600, fontSize: '13px', lineHeight: '16px', color: '#55E5C5' }}>{preview.title}</Typography>
+        <Typography sx={{ fontWeight: 500, fontSize: '15px', lineHeight: '20px', color: '#FFF' }}>{preview.subTitle}</Typography>
+        <Box
+          sx={{
+            paddingTop: '4px',
+            display: 'flex',
+            justifyContent: 'start',
+            color: '#8f9498',
+            fontSize: '13px',
+            alignItems: 'center',
+          }}
+        >
+          <StarIcon sx={{ color: '#55E5C5', height: '17px', width: '17px', mb: '3px', }} />
+          <span>{preview.startValue} | Desktop</span>
+        </Box>
+      </Box>
+    </Box>
+  )
+}
